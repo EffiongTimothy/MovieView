8dcb7c8a93cada560db61e99868d4063bd58e697
timothy <effiongtimothy1@gmail.com>
2023-11-07 00:05:25 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,87 +1,124 @@
-import React from 'react'; 
-import { useState, useEffect } from 'react';
-import './App.css';
-import MovieList from './component/movieList';
-import 'bootstrap/dist/css/bootstrap.min.css'
-import MovieListHeader from './component/movieListHeader';
-import SearchBar from './component/searchField';
-import AddFavourite from './component/AddFavourite';
-import RemoveFavourite from './component/RemoveFavourite';
+import React from "react";
+import { useState, useEffect } from "react";
+import "./App.css";
+import MovieList from "./component/movieList";
+import "bootstrap/dist/css/bootstrap.min.css";
+import MovieListHeader from "./component/movieListHeader";
+import SearchBar from "./component/searchField";
+import AddFavourite from "./component/AddFavourite";
+import RemoveFavourite from "./component/RemoveFavourite";
 
-
-
-const  App = () => {
-  const [movies,setMovies ] = useState([])
-  const [search,setSearch] = useState('')
-  const [favourite,setFavourite] = useState([ ])
- 
-
+const App = () => {
+  const [movies, setMovies] = useState([]);
+  const [search, setSearch] = useState("");
+  const [favourite, setFavourite] = useState([]);
 
   const getMovieRequest = async (search) => {
-    const url = `http://www.omdbapi.com/?s=${search}&apikey=678b9f83`
-    const response = await fetch(url)
-   const responseJson = await response.json() 
-    if (responseJson.Search){
-   setMovies(responseJson.Search)
-   }
-  }
-  useEffect(()=>{
+    const url = `http://www.omdbapi.com/?s=${search}&apikey=678b9f83`;
+    const response = await fetch(url);
+    const responseJson = await response.json();
+    if (responseJson.Search) {
+      setMovies(responseJson.Search);
+    }
+  };
+  useEffect(() => {
     getMovieRequest(search);
-  },[search]); 
+  }, [search]);
 
-  useEffect(()=>{
+  useEffect(() => {
     const favouriteMovies = JSON.parse(
-      localStorage.getItem('react-movie-app-favourites')
-    )
-    setFavourite(favouriteMovies)
-  },[])
+      localStorage.getItem("react-movie-app-favourites")
+    );
+    setFavourite(favouriteMovies);
+  }, []);
 
-  const saveToLocalStorage =(items) => {
-    localStorage.setItem('react-movie-app-favourites',JSON.stringify(items))
-  }
+  const saveToLocalStorage = (items) => {
+    localStorage.setItem("react-movie-app-favourites", JSON.stringify(items));
+  };
 
-  const addFavourite = (movie) =>{
-    const  newFavouriteList = [...favourite,movie];
-    setFavourite(newFavouriteList)
-    saveToLocalStorage(newFavouriteList)
-  }
+  const addFavourite = (movie) => {
+    const newFavouriteList = [...favourite, movie];
+    setFavourite(newFavouriteList);
+    saveToLocalStorage(newFavouriteList);
+  };
 
-  const removeFavourite =(movie) => {
+  const removeFavourite = (movie) => {
     const newFavouriteList = favourite.filter(
       (favourite) => favourite.imdbID !== movie.imdbID
     );
-    setFavourite(newFavouriteList)
-    saveToLocalStorage(newFavouriteList)
-  }
-   
-  return ( 
+    setFavourite(newFavouriteList);
+    saveToLocalStorage(newFavouriteList);
+  };
+
+  return (
     <div className="container-fluid movie-app">
-     <div className='row align-items-center mt-3 mt-4 sticky-top'>
-      <MovieListHeader heading ='Movies'/>
-        <SearchBar search={search} 
-        setSearch={setSearch}/>
-        </div>
+      <div className="row align-items-center  sticky-top">
+        <MovieListHeader heading="Movies" />
+        <SearchBar search={search} setSearch={setSearch} />
+      </div>
       <br></br>
       <br></br>
       <br></br>
       <br></br>
       <br></br>
 
-       <div className='d-flex mb-50 '>
-      <MovieList movies ={movies} 
-      handleFavouritesClick = {addFavourite}
-      favourite={AddFavourite}/>
-       </div>
-       <div className='d-flex align-items-center mt-3'>
-      <MovieListHeader heading ='Favourites'/>
-        </div>
-        <div className='d-flex'> <MovieList movies ={favourite} 
-      handleFavouritesClick = {removeFavourite}
-      favourite={RemoveFavourite}/>
+      <div className="d-flex mb-50 ">
+        <MovieList
+          movies={movies}
+          handleFavouritesClick={addFavourite}
+          favourite={AddFavourite}
+        />
+      </div>
+      <div className="d-flex align-items-center mt-3 fixed">
+        <MovieListHeader heading="Favourites" />
+      </div>
+      <div className="d-flex">
+        {" "}
+        <MovieList
+          movies={favourite}
+          handleFavouritesClick={removeFavourite}
+          favourite={RemoveFavourite}
+        />
+      </div>
     </div>
-    </div>
-       
   );
-}
+};
 
 export default App;
+
+// import React, { useEffect, useState } from 'react';
+
+// const Profile = () => {
+//   const [profileData, setProfileData] = useState(null);
+
+//   useEffect(() => {
+//     fetchProfileData();
+//   }, []);
+
+//   const fetchProfileData = async () => {
+//     try {
+//       const response = await fetch('http://jsonplaceholder.typicode.com/comments');
+//       const data = await response.json();
+//       console.log(data)
+//       setProfileData(data.name);
+//     } catch (error) {
+//       console.log('Error:', error);
+//     }
+//   };
+
+//   return (
+//     <div style={{backgroundColor:"blue"}}>
+//       {profileData ? (
+//         <div>
+//           <h2>Name: {profileData.name}</h2>
+//           <p>Age: {profileData.email}</p>
+//           <p>Gender: {profileData.body}</p>
+//         </div>
+//       ) : (
+//         <p>Loading profile data...</p>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default Profile;
--- src/component/movieList.js
+++ src/component/movieList.js
@@ -1,18 +1,23 @@
 import React from "react";
 
 const MovieList = (props) => {
+    const { movies, handleFavouritesClick } = props;
+
+    if (!movies) {
+      return <p>No movies available.</p>;
+    }
     const Favourite = props.favourite
     return(
         <>
-        {props.movies.map((movie, index) => (
+        {props.movies.map((movie, index) => 
         <div className="image-container d-flex justify-content-start m-4">
             <img src={movie.Poster} alt="poster"></img>
              <div
-             onClick={() =>props.handleFavouritesClick(movie) } className="overlay d-flex align-items-center justify-content-center">  
+             onClick={() => handleFavouritesClick(movie)} className="overlay d-flex align-items-center justify-content-center">  
             <Favourite/>
             </div> 
         </div>
-        ))}
+        )}
         </>
     )
 }
